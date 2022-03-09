609587b8535afe1ba5bcbaabab71d2687283f47f
陳世逢 <fong@fengdeMacBook-Pro.local>
2022-03-09 15:52:05 +0800

    push tw stock

--- fonglinebot/defineWTD.py
+++ fonglinebot/defineWTD.py
@@ -1,14 +1,24 @@
+from twstock import gettwstock
+import pprint
 def wtd(query):
-    if query[0] == '！' or query[0] == '!':
-        ans = 'b'
+    if query[0:2] == '！ ' or query[0:2] == '! ':
+        query = query[2:]
+
+        ans = query
+        return ans
+    elif query[0:2] == 'tw' or query[0:2] == 'TW' or query[0:2] == 'Tw':
+        query = query[3:]
+        query = str(query)
+        query.split()
+        ans = gettwstock(query)
         return ans
     else:
-        ans = 'c'
+        ans = None
         return ans
 
 # if __name__ == '__main__':
 
-#     dis = wtd('ja')
+#     dis = wtd('tw 2330')
 #     print(dis)
 
 #     pass
---
+++ fonglinebot/twstock.py
@@ -0,0 +1,68 @@
+import requests
+import pandas as pd
+import numpy as np
+import datetime
+
+def conv_to_list(obj):
+    '''
+    將物件轉換為list
+    '''
+    if not isinstance(obj, list) :
+        results = [obj]
+    else:
+        results = obj
+    return results
+
+
+def df_conv_col_type(df, cols, to, ignore=False):
+    '''
+    一次轉換多個欄位的dtype
+    '''
+    cols = conv_to_list(cols)
+    for i in range(len(cols)):
+        if ignore :
+            try:
+                df[cols[i]] = df[cols[i]].astype(to)
+            except:
+                print('df_conv_col_type - ' + cols[i] + '轉換錯誤')
+                continue
+        else:
+            df[cols[i]] = df[cols[i]].astype(to)
+    return df
+
+
+def date_get_today(with_time=False):
+    '''
+    取得今日日期，並指定為台北時區
+    '''
+    import pytz
+    central = pytz.timezone('Asia/Taipei')
+    
+    if with_time == True:
+        now = datetime.datetime.now(central)
+    else:
+        now = datetime.datetime.now(central).date()
+    return now
+
+
+def gettwstock(stockID):
+    # 下載證交所資料 ------
+    link = 'https://www.twse.com.tw/exchangeReport/STOCK_DAY_ALL?response=open_data'
+    data = pd.read_csv(link)
+
+    # ['證券代號', '證券名稱', '成交股數', '成交金額', '開盤價',
+    #  '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數']
+    data.columns =  ['證券代號', '證券名稱', '成交股數', '成交金額', '開盤價',
+    '最高價', '最低價', '收盤價', '漲跌價差', '成交筆數']  
+    # 標註今日日期
+    data['日期'] = date_get_today()
+    stock_num = str(stockID)
+    
+    cols = data.columns.tolist()
+    cols = cols[-1:] + cols[:-1]
+    data = data[cols]
+    # 除了證券代號外，其他欄位都是str，且部份資料中有''
+    data = data.replace('', np.nan, regex=True)
+    ans = (data.loc[(data['證券代號'] == stock_num)])
+    return ans
+    
