e3b40e5771326fced37e9eb662e520d91d254fac
陳世逢 <fong@fengdeMacBook-Pro.local>
2022-03-09 18:35:08 +0800

    us stocks list

--- fonglinebot/stocksAPI.py
+++ fonglinebot/stocksAPI.py
@@ -17,5 +17,5 @@ def stockapi(ID):
     finnhub_client = finnhub.Client(api_key="c8k6sqaad3i8fk1kn8ag")
     ans = finnhub_client.quote(ID)
     ans.pop('t')
-    all = '現在價格 : ' + str(ans['c']) + '\n變動價格 : ' + str(ans['d']) + '\n變動趴數 : ' + str(ans['dp']) + '\n今日最高 : ' + str(ans['h']) + '\n今日最低 : '+ str(ans['l']) + '\n開市價格 : '+ str(ans['o']) + '\n上次閉市 : '+ str(ans['pc'])
+    all = '個股代號 : ' + ID + '\n現在價格 : ' + str(ans['c']) + '\n變動價格 : ' + str(ans['d']) + '\n變動趴數 : ' + str(ans['dp']) + '\n今日最高 : ' + str(ans['h']) + '\n今日最低 : '+ str(ans['l']) + '\n開市價格 : '+ str(ans['o']) + '\n上次閉市 : '+ str(ans['pc'])
     return all
