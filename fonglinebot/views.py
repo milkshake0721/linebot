c5e397199f0104b2bf60cfd76a55da9a89d53927
陳世逢 <fong@fengdeMacBook-Pro.local>
2022-03-09 12:44:26 +0800

    make it better4

--- fonglinebot/views.py
+++ fonglinebot/views.py
@@ -7,7 +7,7 @@ from django.conf import settings
 from linebot import LineBotApi, WebhookParser
 from linebot.exceptions import InvalidSignatureError, LineBotApiError
 from linebot.models import MessageEvent, TextSendMessage
-from .scraper import IFoodie
+
  
 line_bot_api = LineBotApi(settings.LINE_CHANNEL_ACCESS_TOKEN)
 parser = WebhookParser(settings.LINE_CHANNEL_SECRET)
