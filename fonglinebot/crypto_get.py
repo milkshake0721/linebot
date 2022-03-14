a2d46fd30eb75fe3136a40e1c7ae85e3e080b233
Your Name <simon34345@gmail.com>
2022-03-14 17:17:41 +0800

    roomid

--- fonglinebot/crypto_get.py
+++ fonglinebot/crypto_get.py
@@ -5,7 +5,7 @@ def crypto(coin):
     # url = 'http://ftx.com/api/markets/ETH/USDT'
     url_history = 'http://ftx.com/api/markets/' + coin + '/USD/candles?resolution=2592000'
     r = requests.post(url)
-    if r.json()['success'] == 'false':
+    if r.json()['success'] == False:
         all = gate_io(coin)
 
     else:
