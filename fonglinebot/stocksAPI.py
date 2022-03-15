12eb3eafb270d071853902714d99ed06ea05c716
Your Name <simon34345@gmail.com>
2022-03-15 11:48:23 +0800

    roomid

--- fonglinebot/stocksAPI.py
+++ fonglinebot/stocksAPI.py
@@ -1,3 +1,4 @@
+from re import A
 import finnhub
 import pprint
 import datetime
@@ -13,13 +14,19 @@ def stockapi(ID):
     tomarrow = tomarrow.strftime("%Y-%m-%d")
     finnhub_client = finnhub.Client(api_key="c8k6sqaad3i8fk1kn8ag")
     ans = finnhub_client.quote(ID)
+    # print(ans)
     news = finnhub_client.company_news(ID, _from = yesterday , to = today)
+    print(news)
+    if news == []:
+        news = [{'headline':'','url':''}]
+        news[0]['headline'] = 'N/A'
+        news[0]['url'] = 'N/A'
     ans.pop('t')
     # all = news[0]['url']
     all = ID + ' ( ' +str(ans['dp']) + '% )\n現在價格 : ' + str(ans['c']) + '\n-------------------\n\n變動價格 : ' + str(ans['d']) + '\n今日最高 : ' + str(ans['h']) + '\n今日最低 : '+ str(ans['l']) + '\n開市價格 : '+ str(ans['o']) + '\n上次閉市 : '+ str(ans['pc']) + '\n\n-------------------\n新聞 : '+ news[0]['headline']+ '\n' + news[0]['url']
     return all
 
-# print(stockapi('AAPL'))
+print(stockapi('hl'))
 
 def currency():
     finnhub_client = finnhub.Client(api_key="sandbox_c8k6qfaad3i8fk1kn7c0")
@@ -33,4 +40,4 @@ def currency():
     all = '美金 : ' + usd + '\n日幣 : '+ jpy +'\n歐元 : ' + eur + '\n人民幣:' + rmb  + '\n盧布 : ' + rub
     # print('美金 : ' + usd + '\n日幣 : '+ jpy +'\n歐元 : ' + eur )
     return all
-print(currency())
+# print(currency())
