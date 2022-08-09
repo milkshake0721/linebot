369453bb046674f60e6dd23799f62beacbc12644
Your Name <simon34345@gmail.com>
2022-08-09 15:46:08 +0800

    update to 22

--- fonglinebot/views.py
+++ fonglinebot/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseBadRequest, HttpResponseForbidden
 from django.views.decorators.csrf import csrf_exempt
 from django.conf import settings
