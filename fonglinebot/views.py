e5c60b22436294218ddb21a0a166f653d33f6ddc
Your Name <simon34345@gmail.com>
2022-08-09 16:34:45 +0800

    push usdt

--- fonglinebot/views.py
+++ fonglinebot/views.py
@@ -160,7 +160,7 @@ def callback(request):
                     ans = usdt()
                     line_bot_api.push_message(  # 回復訊息文字
                         to = event.reply_token,
-                        message = ans
+                        messages = ans
                     )
                 if  ask[0:3] == '貸出 ' :
                     ans = spot_margin(ask)
